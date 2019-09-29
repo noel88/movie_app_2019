9ca9145d2ad548c7b86861ff1ed4258388148047
N <xiah0526@gmail.com>
2019-09-29 16:57:23 +0900

    Updates

---
+++ precache-manifest.a9a44a0e52eacb6d43b42245aa06c8ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbf1a4e468f611cd46353d00b86fb69",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "305e62e4ca68a3a46105",
+    "url": "/movie_app_2019/static/css/main.794568c0.chunk.css"
+  },
+  {
+    "revision": "18688e249930b5fb4515",
+    "url": "/movie_app_2019/static/js/2.3578f9fe.chunk.js"
+  },
+  {
+    "revision": "305e62e4ca68a3a46105",
+    "url": "/movie_app_2019/static/js/main.9a13adfa.chunk.js"
+  },
+  {
+    "revision": "81099d8470244db95110",
+    "url": "/movie_app_2019/static/js/runtime-main.f4904fd8.js"
+  }
+]);
